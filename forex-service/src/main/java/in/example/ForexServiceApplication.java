128eeb433a6a8df0ece72477e37026f104ead174
rahul-gaidhane <rahulgaidhane98@gmail.com>
2022-05-23 12:34:21 +0530

    register forex service with eureka server

--- forex-service/src/main/java/in/example/ForexServiceApplication.java
+++ forex-service/src/main/java/in/example/ForexServiceApplication.java
@@ -5,8 +5,10 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ForexServiceApplication {
 
